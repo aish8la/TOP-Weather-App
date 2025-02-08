74100d97ad179591ef354937f35fd14e1b24d7ed
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-02-09 03:54:11 +0500

    Add timezone support with @date-fns/tz and refactor weather fetching logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import { addDays, format } from "date-fns";
+import { addDays, format, addHours, roundToNearestHours } from "date-fns";
 
 const apiObj = {
     "url": "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/",
     "apiKey": "AQDDMAMVYVMQ3HDRU8QNG3XF3",
     "unitGroup": "metric",
-    "locationString": "Male",
+    "locationString": "India",
     apiString() {
         const unit = "unitGroup="+(this.unitGroup || "metric");
         const location = this.locationString || "Male";
@@ -12,9 +12,41 @@ const apiObj = {
     },
 }
 
-export default async function simpleFetch() {
-    const weather = await fetch(apiObj.apiString());
-    const parsedWeather = await weather.json();
+let weatherData = {};
 
-    console.log(parsedWeather);
+export default async function fetchWeather() {
+    const weather = await fetch(apiObj.apiString());
+    weatherData = await weather.json();
+
+    console.log(weatherData);
 }
+
+function processData(parsedJSON) {
+    let jsonObj = parsedJSON;
+
+    let requiredData = {
+        address: jsonObj.resolvedAddress,
+        timezone: jsonObj.timezone,
+        currentConditions: {
+            conditions: jsonObj.conditions,
+            datetime: jsonObj.datetime,
+            feelslike: jsonObj.feelslike,
+            humidity: jsonObj.humidity,
+            icon: jsonObj.icon,
+            temperature: jsonObj.temp,
+            windspeed: jsonObj.windspeed
+        },
+        days: [],
+        hours: [],
+    }
+    
+    for (let i = 0; i < 7; i++) {
+        const newObj = {
+            date: jsonObj.days[i].datetime,
+            temperature: jsonObj.days[i].temp,
+            conditions: jsonObj.days[i].conditions, 
+        }
+        requiredData.days.push(newObj);
+    }
+}
+
